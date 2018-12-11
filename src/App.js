a8a3ccb76be9b2b092de616b0e44f23b2868b3ae
tayyab <tayyab.razzaq@arbisoft.com>
2018-12-11 11:13:01 +0500

    feat: Store added and configured

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import routes from "./routes";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class WouldYouRatherApp extends Component {
+	render() {
+		return (
+			<div className="app">
+				Hello World
+				{routes}
+			</div>
+		);
+	}
 }
 
-export default App;
+export default WouldYouRatherApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import WouldYouRatherApp from './App';
 import * as serviceWorker from './serviceWorker';
+import {createBrowserHistory} from "history";
+import {Router} from "react-router-dom";
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function App (props) {
+	return (
+		<Provider store={store} {...props}>
+			<Router history={createBrowserHistory()}>
+				<WouldYouRatherApp/>
+			</Router>
+		</Provider>
+	);
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
