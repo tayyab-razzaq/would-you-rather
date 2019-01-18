ead11ca6dd4eac605a54a94d5a7675a32876caeb
tayyab <tayyab.razzaq@arbisoft.com>
2019-01-19 00:01:05 +0500

    Updates

---
+++ precache-manifest.dc93b9edcfe60242cbcbc234b0de96e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10921595715c76a00edb",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/main.dfe4a376.chunk.css"
+  },
+  {
+    "revision": "10921595715c76a00edb",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/main.10921595.chunk.js"
+  },
+  {
+    "revision": "a602c927c04b149cad3f",
+    "url": "/reactnd-project-would-you-rather-starter/static/css/1.349c237d.chunk.css"
+  },
+  {
+    "revision": "a602c927c04b149cad3f",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/1.a602c927.chunk.js"
+  },
+  {
+    "revision": "efc043941366931b791d",
+    "url": "/reactnd-project-would-you-rather-starter/static/js/runtime~main.efc04394.js"
+  },
+  {
+    "revision": "06e469f85d0dd46519a05e273901dacb",
+    "url": "/reactnd-project-would-you-rather-starter/static/media/react-redux.06e469f8.jpg"
+  },
+  {
+    "revision": "e635e55265147377ef597e7d98464f36",
+    "url": "/reactnd-project-would-you-rather-starter/index.html"
+  }
+];
